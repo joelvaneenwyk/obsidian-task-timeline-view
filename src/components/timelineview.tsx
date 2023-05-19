fdce51a79b367ec5adf889fffc8c26096f25b0bb
zhuwenq <zhuwenqa.outlook.com>
2023-05-19 16:42:04 +0800

    refactor: replace the channel for task list transfer from context to props and passing entryOnDate to context

--- src/components/timelineview.tsx
+++ src/components/timelineview.tsx
@@ -8,6 +8,7 @@ import { YearView } from './yearview';
 
 const defaultTimelineProps = {
     userOptions: {} as UserOption,
+    taskList: [] as TaskDataModel[]
 }
 const defaultTimelineStates = {
     filter: "" as string,
@@ -48,115 +49,116 @@ export class TimelineView extends React.Component<TimelineProps, TimelineStates>
     }
 
     render(): React.ReactNode {
+        const involvedDates: Set<string> = new Set();
+        const taskList = this.props.taskList;
+        taskList.forEach((t: TaskDataModel) => {
+            t.due && involvedDates.add(t.due.format(innerDateFormat));
+            t.scheduled && involvedDates.add(t.scheduled.format(innerDateFormat));
+            t.created && involvedDates.add(t.created.format(innerDateFormat));
+            t.start && involvedDates.add(t.start.format(innerDateFormat));
+            t.completion && involvedDates.add(t.completion.format(innerDateFormat));
+            t.dates.forEach((d: Moment, k: string) => {
+                involvedDates.add(d.format(innerDateFormat));
+            });
+        })
+
+        if (!involvedDates.has(moment().format(innerDateFormat)))
+            involvedDates.add(moment().format(innerDateFormat));
+
+        const sortedDatas = [...involvedDates].sort();
+        const earliestYear: number = +moment(sortedDatas[0].toString()).format("YYYY");
+        const latestYear: number = +moment(sortedDatas[sortedDatas.length - 1].toString()).format("YYYY");
+        const years = Array.from({ length: latestYear - earliestYear + 1 }, (_, i) => i + earliestYear);
+        const firstDay = sortedDatas.first();
+        const lastDay = sortedDatas.last();
+
+        const taskOfToday = taskList.filter(TaskMapable.filterDate(moment()));
+        const overdueCount: number = taskOfToday.filter(t => t.status === TaskStatus.overdue).length;
+        const unplannedCount: number = taskOfToday.filter(t => t.status === TaskStatus.unplanned).length;
+        const completedCount: number = taskOfToday.filter(t => t.status === TaskStatus.done).length;
+        const cancelledCount: number = taskOfToday.filter(t => t.status === TaskStatus.cancelled).length;
+        // .due, .scheduled, .process, .start
+        const todoCount: number = taskOfToday.length - unplannedCount - completedCount - cancelledCount - overdueCount;
+
+        const styles = new Array<string>;
+        if (!this.props.userOptions.useCounters) styles.push("noCounters");
+        if (!this.props.userOptions.useQuickEntry) styles.push("noQuickEntry");
+        if (!this.props.userOptions.useYearHeader) styles.push("noYear");
+        if (!this.props.userOptions.useCompletedTasks) styles.push("noDone");
+        if (!this.props.userOptions.useFileBadge &&
+            !this.props.userOptions.usePriority &&
+            !this.props.userOptions.useRecurrence &&
+            !this.props.userOptions.useRelative &&
+            !this.props.userOptions.useSection &&
+            !this.props.userOptions.useTags) styles.push("noInfo");
+        else {
+            if (!this.props.userOptions.useFileBadge) styles.push("noFile");
+            if (!this.props.userOptions.usePriority) styles.push("noPriority");
+            if (!this.props.userOptions.useRecurrence) styles.push("noRepeat");
+            if (!this.props.userOptions.useRelative) styles.push("noRelative");
+            if (!this.props.userOptions.useSection) styles.push("noHeader");
+            if (!this.props.userOptions.useTags) styles.push("noTag");
+        }
+
+        const quickEntryFiles = new Set(this.props.userOptions.taskFiles);
+        if (this.props.userOptions.inbox && this.props.userOptions.inbox !== '')
+            quickEntryFiles.add(this.props.userOptions.inbox);
+        const dailyNoteFileName = moment().format(this.props.userOptions.dailyNoteFormat) + ".md";
+        const daileNoteFolder =
+            this.props.userOptions.dailyNoteFolder === '' ?
+                '' : this.props.userOptions.dailyNoteFolder.endsWith('/') ?
+                    this.props.userOptions.dailyNoteFolder : this.props.userOptions.dailyNoteFolder + '/';
+        if (this.props.userOptions.dailyNoteFormat && this.props.userOptions.dailyNoteFormat !== '')
+            quickEntryFiles.add(daileNoteFolder + dailyNoteFileName);
 
         return (
-            <TaskListContext.Consumer>{({ taskList }) => {
-                const involvedDates: Set<string> = new Set();
-
-                taskList.forEach((t: TaskDataModel) => {
-                    t.due && involvedDates.add(t.due.format(innerDateFormat));
-                    t.scheduled && involvedDates.add(t.scheduled.format(innerDateFormat));
-                    t.created && involvedDates.add(t.created.format(innerDateFormat));
-                    t.start && involvedDates.add(t.start.format(innerDateFormat));
-                    t.completion && involvedDates.add(t.completion.format(innerDateFormat));
-                    t.dates.forEach((d: Moment, k: string) => {
-                        involvedDates.add(d.format(innerDateFormat));
-                    });
-                })
-
-                if (!involvedDates.has(moment().format(innerDateFormat)))
-                    involvedDates.add(moment().format(innerDateFormat));
-
-                const sortedDatas = [...involvedDates].sort();
-                const earliestYear: number = +moment(sortedDatas[0].toString()).format("YYYY");
-                const latestYear: number = +moment(sortedDatas[sortedDatas.length - 1].toString()).format("YYYY");
-                const years = Array.from({ length: latestYear - earliestYear + 1 }, (_, i) => i + earliestYear);
-
-                const taskOfToday = taskList.filter(TaskMapable.filterDate(moment()));
-                const overdueCount: number = taskOfToday.filter(t => t.status === TaskStatus.overdue).length;
-                const unplannedCount: number = taskOfToday.filter(t => t.status === TaskStatus.unplanned).length;
-                const completedCount: number = taskOfToday.filter(t => t.status === TaskStatus.done).length;
-                const cancelledCount: number = taskOfToday.filter(t => t.status === TaskStatus.cancelled).length;
-                // .due, .scheduled, .process, .start
-                const todoCount: number = taskOfToday.length - unplannedCount - completedCount - cancelledCount - overdueCount;
-
-                const styles = new Array<string>;
-                if (!this.props.userOptions.useCounters) styles.push("noCounters");
-                if (!this.props.userOptions.useQuickEntry) styles.push("noQuickEntry");
-                if (!this.props.userOptions.useYearHeader) styles.push("noYear");
-                if (!this.props.userOptions.useCompletedTasks) styles.push("noDone");
-                if (!this.props.userOptions.useFileBadge &&
-                    !this.props.userOptions.usePriority &&
-                    !this.props.userOptions.useRecurrence &&
-                    !this.props.userOptions.useRelative &&
-                    !this.props.userOptions.useSection &&
-                    !this.props.userOptions.useTags) styles.push("noInfo");
-                else {
-                    if (!this.props.userOptions.useFileBadge) styles.push("noFile");
-                    if (!this.props.userOptions.usePriority) styles.push("noPriority");
-                    if (!this.props.userOptions.useRecurrence) styles.push("noRepeat");
-                    if (!this.props.userOptions.useRelative) styles.push("noRelative");
-                    if (!this.props.userOptions.useSection) styles.push("noHeader");
-                    if (!this.props.userOptions.useTags) styles.push("noTag");
-                }
-
-                const quickEntryFiles = new Set(this.props.userOptions.taskFiles);
-                if (this.props.userOptions.inbox && this.props.userOptions.inbox !== '')
-                    quickEntryFiles.add(this.props.userOptions.inbox);
-                const dailyNoteFileName = moment().format(this.props.userOptions.dailyNoteFormat) + ".md";
-                const daileNoteFolder =
-                    this.props.userOptions.dailyNoteFolder === '' ?
-                        '' : this.props.userOptions.dailyNoteFolder.endsWith('/') ?
-                            this.props.userOptions.dailyNoteFolder : this.props.userOptions.dailyNoteFolder + '/';
-                if (this.props.userOptions.dailyNoteFormat && this.props.userOptions.dailyNoteFormat !== '')
-                    quickEntryFiles.add(daileNoteFolder + dailyNoteFileName);
-
-                return (
-                    <div className={`taskido ${[...new Set(styles)].join(" ")} ${this.state.filter} ${this.state.todayFocus ? "todayFocus" : ""}`}
-                        id={`taskido${(new Date()).getTime()}`}>
-                        <TodayFocusEventHandlersContext.Provider value={{ handleTodayFocusClick: this.handleTodayFocus }}>
-                            <UserOptionContext.Provider value={{
-                                hideTags: this.props.userOptions.hideTags,
-                                tagPalette: this.props.userOptions.tagColorPalette,
-                                dateFormat: this.props.userOptions.dateFormat,
-                                taskFiles: [...quickEntryFiles],
-                                select: this.props.userOptions.inbox,
-                                counters: [
+            <div className={`taskido ${[...new Set(styles)].join(" ")} ${this.state.filter} ${this.state.todayFocus ? "todayFocus" : ""}`}
+                id={`taskido${(new Date()).getTime()}`
+                }>
+                <TodayFocusEventHandlersContext.Provider value={{ handleTodayFocusClick: this.handleTodayFocus }}>
+                    <UserOptionContext.Provider value={{
+                        hideTags: this.props.userOptions.hideTags,
+                        tagPalette: this.props.userOptions.tagColorPalette,
+                        dateFormat: this.props.userOptions.dateFormat,
+                        taskFiles: [...quickEntryFiles],
+                        select: this.props.userOptions.inbox,
+                        counters: [
+                            {
+                                onClick: () => { this.handleCounterFilterClick('todoFilter') },
+                                cnt: todoCount,
+                                label: "Todo",
+                                id: "todo",
+                                ariaLabel: "Filter Todo Tasks"
+                            }, {
+                                onClick: () => { this.handleCounterFilterClick('overdueFilter') },
+                                cnt: overdueCount,
+                                id: "overdue",
+                                label: "Overdue",
+                                ariaLabel: "Filter Overdue Tasks"
+                            }, {
+                                onClick: () => { this.handleCounterFilterClick('unplannedFilter') },
+                                cnt: unplannedCount,
+                                id: "unplanned",
+                                label: "Unplanned",
+                                ariaLabel: "Filter Unplanned Tasks"
+                            }
+                        ]
+                    }}>
+                        <span>
+                            {years.map((y, i) => (
+                                <TaskListContext.Provider value={
                                     {
-                                        onClick: () => { this.handleCounterFilterClick('todoFilter') },
-                                        cnt: todoCount,
-                                        label: "Todo",
-                                        id: "todo",
-                                        ariaLabel: "Filter Todo Tasks"
-                                    }, {
-                                        onClick: () => { this.handleCounterFilterClick('overdueFilter') },
-                                        cnt: overdueCount,
-                                        id: "overdue",
-                                        label: "Overdue",
-                                        ariaLabel: "Filter Overdue Tasks"
-                                    }, {
-                                        onClick: () => { this.handleCounterFilterClick('unplannedFilter') },
-                                        cnt: unplannedCount,
-                                        id: "unplanned",
-                                        label: "Unplanned",
-                                        ariaLabel: "Filter Unplanned Tasks"
+                                        taskList: taskList.filter(TaskMapable.filterYear(moment().year(y))),
+                                        entryOnDate: this.props.userOptions.entryPosition === "top" ? firstDay! :
+                                            this.props.userOptions.entryPosition === "bottom" ? lastDay! : moment().toString(),
                                     }
-                                ]
-                            }}>
-                                <span>
-                                    {years.map((y, i) => (
-                                        <TaskListContext.Provider value={
-                                            { taskList: taskList.filter(TaskMapable.filterYear(moment().year(y))) }
-                                        } key={i}>
-                                            <YearView year={y} key={y} />
-                                        </TaskListContext.Provider>
-                                    ))}
-                                </span>
-                            </UserOptionContext.Provider>
-                        </TodayFocusEventHandlersContext.Provider>
-                    </div>)
-            }}
-            </TaskListContext.Consumer>
-        );
+                                } key={i}>
+                                    <YearView year={y} key={y} />
+                                </TaskListContext.Provider>
+                            ))}
+                        </span>
+                    </UserOptionContext.Provider>
+                </TodayFocusEventHandlersContext.Provider>
+            </div >)
     }
 }
