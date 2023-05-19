39d705a93d49cca637f7431fbfe033b340d3d979
zhuwenq <zhuwenqa.outlook.com>
2023-05-19 16:43:10 +0800

    remove unused code and passing entryOnDate to context

--- src/components/yearview.tsx
+++ src/components/yearview.tsx
@@ -11,7 +11,7 @@ type YearViewProps = Readonly<typeof defaultYearViewProps>;
 export class YearView extends React.Component<YearViewProps> {
     render(): React.ReactNode {
         return (
-            <TaskListContext.Consumer>{({ taskList }) => {
+            <TaskListContext.Consumer>{({ taskList, entryOnDate }) => {
                 const tasksOfThisYear = taskList;
                 const daysOfThisYear: Set<string> = new Set();
                 tasksOfThisYear.forEach((t) => {
@@ -24,23 +24,20 @@ export class YearView extends React.Component<YearViewProps> {
                         daysOfThisYear.add(d.format(innerDateFormat));
                     });
                 })
-                if (this.props.year === moment().year()) {
-                    /**
-                     * Today is promised to be in the date list
-                     */
-                    daysOfThisYear.add(moment().format(innerDateFormat));
-                }
                 return (
                     <div>
                         <YearHeader year={this.props.year} dataTypes={[...new Set(tasksOfThisYear.map(t => t.status))]} />
-                        {[...daysOfThisYear].sort().map((d, i) => {
-                            const tasksOfThisDate = tasksOfThisYear.filter(TaskMapable.filterDate(moment(d)));
-                            return (
-                                <TaskListContext.Provider value={{ taskList: tasksOfThisDate }} key={i}>
-                                    <DateView date={moment(d)} key={i} />
-                                </TaskListContext.Provider>
-                            )
-                        })}
+                        {[...daysOfThisYear]
+                            .filter(d => moment(d).year() === this.props.year)
+                            .sort()
+                            .map((d, i) => {
+                                const tasksOfThisDate = tasksOfThisYear.filter(TaskMapable.filterDate(moment(d)));
+                                return (
+                                    <TaskListContext.Provider value={{ taskList: tasksOfThisDate, entryOnDate: entryOnDate }} key={i}>
+                                        <DateView date={moment(d)} key={i} />
+                                    </TaskListContext.Provider>
+                                )
+                            })}
                     </div>)
             }}
             </TaskListContext.Consumer>
